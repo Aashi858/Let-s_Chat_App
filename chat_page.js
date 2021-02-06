8cf94a40f561f5a52f46f43c3ede16d20a492696
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-02-06 21:42:55 +0530

    Add files via upload

--- chat_page.js
+++ chat_page.js
@@ -12,15 +12,28 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  
+
   username = localStorage.getItem("User_Name");
-  room_Name = localStorage.getItem("Room_Name");
+  room_name = localStorage.getItem("Room_Name");
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
        firebase_message_id = childKey;
        message_data = childData;
 //Start code
-
+    console.log(firebase_message_id);
+    console.log(message_data);
+    name_data = message_data['Name'];
+    console.log(name_data);
+    message = message_data['Message'];
+    console.log(message)
+    like = message_data['Like'];
+    console.log(like);
+    name_tag = " <h4>"+  name_data + " <img id = 'user_tick' src = 'tick.jpg'> </h4>";
+    message_tag = "<h4 class = 'message_h4'>" + message + "<h4>";
+    like_button = "<button class = 'btn btn-warning' id = " + firebase_message_id + "value =" + like + "onclick = 'updateLike(this.id)'>";
+    span_tag = "<span class = 'glyphicon glyphicon-thumbs-up'> Like " + like + "</span> </button> <hr>";
+    row = name_tag + message_tag + like_button + span_tag ;
+    document.getElementById("output").innerHTML += row ;
 //End code
     } });  }); }
 getData();
@@ -32,10 +45,21 @@ function logout(){
 }
 function send(){
     msg = document.getElementById("msg").value;
-    firebase.database().ref(room_Name).push({
-          Like : 0,
-          Message : msg,
-          Name : username
+    firebase.database().ref(room_name).push({
+          like : 0,
+          message : msg,
+          name : username
     });
     document.getElementById("msg").value = "";
+}
+function updateLike(message_id){
+    console.log(message_id);
+    button_id = message_id;
+    number_of_like = document.getElementById(button_id).value;
+    update_like = Number(number_of_like) + 1;
+    console.log(update_like);
+    document.getElementById(update_like).value
+    firebase.database().ref(room_name).child(message_id).update({
+          like : update_like
+    });
 }
